82af97f5b699414924f4170f68f9cbe090c40b6e
Aofei Sheng <aofei@aofeisheng.com>
2018-08-17 14:20:13 +0800

    refactor: make gases configurable

--- logger.go
+++ logger.go
@@ -6,41 +6,49 @@ import (
 	"github.com/sheng/air"
 )
 
-// msg is the logger's main output message.
-const msg = "finished request-response cycle"
+// GasConfig is a set of configurations for the `Gas()`.
+type GasConfig struct {
+	Message string
+}
 
-// Gas is used to log every request.
-func Gas(next air.Handler) air.Handler {
-	return func(req *air.Request, res *air.Response) error {
-		startTime := time.Now()
-		err := next(req, res)
-		endTime := time.Now()
+// Gas returns a `air.Gas` that is used to log ervery request based on the gc.
+func Gas(gc GasConfig) air.Gas {
+	if gc.Message == "" {
+		gc.Message = "finished request-response cycle"
+	}
 
-		extras := map[string]interface{}{
-			"remote_addr": req.RemoteAddr,
-			"method":      req.Method,
-			"path":        req.URL.Path,
-			"start_time":  startTime.UnixNano(),
-			"end_time":    endTime.UnixNano(),
-			"latency":     endTime.Sub(startTime).String(),
-			"bytes_in":    req.ContentLength,
-			"bytes_out":   res.ContentLength,
-		}
+	return func(next air.Handler) air.Handler {
+		return func(req *air.Request, res *air.Response) error {
+			startTime := time.Now()
+			err := next(req, res)
+			endTime := time.Now()
 
-		if err != nil {
-			if e, ok := err.(*air.Error); ok {
-				extras["status_code"] = e.Code
-			} else {
-				extras["status_code"] = 500
+			extras := map[string]interface{}{
+				"remote_addr": req.RemoteAddr,
+				"method":      req.Method,
+				"path":        req.URL.Path,
+				"start_time":  startTime.UnixNano(),
+				"end_time":    endTime.UnixNano(),
+				"latency":     endTime.Sub(startTime).String(),
+				"bytes_in":    req.ContentLength,
+				"bytes_out":   res.ContentLength,
 			}
 
-			extras["error"] = err.Error()
-			air.ERROR(msg, extras)
-		} else {
-			extras["status_code"] = res.StatusCode
-			air.INFO(msg, extras)
-		}
+			if err != nil {
+				if e, ok := err.(*air.Error); ok {
+					extras["status_code"] = e.Code
+				} else {
+					extras["status_code"] = 500
+				}
 
-		return err
+				extras["error"] = err.Error()
+				air.ERROR(gc.Message, extras)
+			} else {
+				extras["status_code"] = res.StatusCode
+				air.INFO(gc.Message, extras)
+			}
+
+			return err
+		}
 	}
 }
